7a5b7ca5f5695b64b7c132ba3260848c5ec05644
Alex <malyshef.alexandr@gmail.com>
2023-02-27 21:20:59 +0300

    first3

---
+++ basics1/basics_01.py
@@ -0,0 +1,11 @@
+my_int = 22
+print(my_int)
+
+my_float = 10.5
+print(my_float)
+
+my_str = "word"
+print(my_str)
+
+my_bool = True
+print(my_bool)
---
+++ basics1/basics_03.py
@@ -0,0 +1,7 @@
+age_1 = 22
+age_2 = 35
+age_3 = 81
+age = (age_1 + age_2 + age_3)
+print(age)
+age_middle = ((age_1 + age_2 + age_3) / 3)
+print(age_middle)
