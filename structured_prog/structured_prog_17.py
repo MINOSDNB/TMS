40cd062e88309a5b66f404ce8b0a553249219200
Alex <malyshef.alexandr@gmail.com>
2023-03-23 13:52:46 +0300

    structured_prog1-22

--- structured_prog/structured_prog_17.py
+++ structured_prog/structured_prog_17.py
@@ -1,3 +1,9 @@
+'''
+В массиве целых чисел с количеством элементов 19
+определить максимальное число и заменить им все четные по значению элементы.
+'''
+
+
 from random import randint
 
 arr = [randint(1, 20) for _ in range(19)]
@@ -6,6 +12,7 @@ print(arr, len(arr))
 max_number = max(arr)
 print(max_number)
 print(f'min - {min(arr)}')
+
 
 max_number_oun_solution = arr[0]
 for i in arr:
