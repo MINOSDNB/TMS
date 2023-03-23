8cda38c9d7bb2e22afcdda51722e4d9a8d46fda1
Alex <malyshef.alexandr@gmail.com>
2023-03-23 13:34:14 +0300

    funcs1-2

---
+++ funcs/funcs_01.py
@@ -0,0 +1,12 @@
+def my_funct(name):
+    print(f'Hello {name}')
+
+
+def main():
+    arr = ['Boris', 'Tony', 'Charlie', 'Tureckiy', 'Valera']
+    for name in arr:
+        my_funct(name)
+
+
+if __name__ == '__main__':
+    main()
